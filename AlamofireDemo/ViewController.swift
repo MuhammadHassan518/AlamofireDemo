0d9a1f354b35945b725d5d89813d0f36127ec8b9
Muhammad Hassan <MuhammadHassan518@gmail.com>
2022-03-17 18:24:21 +0500

    using link get data send and get data from server

--- AlamofireDemo/ViewController.swift
+++ AlamofireDemo/ViewController.swift
@@ -6,14 +6,54 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
+    // text boxes and button insilization
+    @IBOutlet weak var txtEmail: UITextField!
+    @IBOutlet weak var txtPassword: UITextField!
+    @IBOutlet weak var btnLogin: UIButton!
+         
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
+    
+    func postMethod() {
+        let params: Parameters = [
+            "email": txtEmail.text!,
+            "password": txtPassword.text!, ]
+        
+        AF.request("http://198.101.12.193:30000/auth/token", method: .post,  parameters: params, encoding: JSONEncoding.default)
+                .responseJSON { response in
+                    switch response.result {
+                    case .success(let value):
+                        if let json = value as? [String: Any] {
+                            print(json["access_token"] as? String ?? String())
+                            let newtoken = json["access_token"] as? String
+                            
+                            if newtoken != nil
+                            { self.alertcall(msg: "success") }
+                            
+                            else
+                            { self.alertcall(msg: "failure") }
+                        }
+                    case .failure(let error):
+                        print(error)
+            }
+        }
+    }
+    
+    @IBAction func btnLoginClicked(_ sender: Any) { postMethod() }
+    
+    
+    func alertcall(msg:String)
+    {
+        let alert = UIAlertController(title: "Alert", message: msg, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default))
+        self.present(alert, animated: true, completion: nil)
+        
+    }
 
 }
 
